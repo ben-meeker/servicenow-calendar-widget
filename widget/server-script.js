316770e958d2608ad31100027365d11cdee04b55
Ben Meeker <ben@theboyscommittee.com>
2024-09-08 16:42:18 -0600

    Updated readme and server script file to be use incident as example

--- widget/server-script.js
+++ widget/server-script.js
@@ -1,8 +1,8 @@
 (function() {
     data.eventList=[];
-	
+    
     // Get events from table
-    var events = new GlideRecord('some table')
+    var events = new GlideRecord('incident')
     // Add queries to narrow down data
     events.addQuery('date>=javascript:gs.beginningOfLastMonth()')
     events.query()
@@ -10,18 +10,23 @@
         var eventJSON = {};
         
         // Set event JSON fields with useful data
-        eventJSON.sys_id = events.getValue('sys_id');
-        eventJSON.venue = events.getValue('venue');
-        eventJSON.title = events.getValue('event_name');
-        eventJSON.date = events.getValue('date');
+    
+        // Set title of event
+        eventJSON.title = events.getValue('short_description');
+        // Set date field to tell calendar when to display event
+        eventJSON.date = events.getValue('due_date');
+        // Set allDay to true to remove time display on event in calendar
         eventJSON.allDay = true;
+
+        // Set custom fields for specific use cases
+        eventJSON.sys_id = events.getValue('sys_id');
+        eventJSON.assigned_to = events.getValue('assigned_to');
         
         // Set additional fields based on criteria
-        if (events.getValue('u_available_tickets') == false) {
+        if (events.getValue('assigned_to') == false) {
             eventJSON.color = 'white';
             eventJSON.textColor = '#344C38';
             eventJSON.borderColor = '#344C38';
-            eventJSON.available_tickets = false;
         }
             
         data.eventList.push(eventJSON);
